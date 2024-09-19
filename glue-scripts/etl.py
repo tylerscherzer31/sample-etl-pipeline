cd0dbb86a91e551bf26761ad8285246495be3538
tylerscherzer31 <30841349+tylerscherzer31@users.noreply.github.com>
2024-09-18 22:04:32 -0400

    nfl api changes

--- glue-scripts/etl.py
+++ glue-scripts/etl.py
@@ -1,4 +1,10 @@
 import sys
+import requests
+
+from pyspark.sql import SparkSession
+from pyspark.sql import Row
+from pyspark.sql.functions import explode, col
+
 from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
@@ -13,8 +19,62 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-print("HELLO FROM GLUE!")
 
+def get_data():
+    # Initialize Spark session
+    spark = SparkSession.builder.appName("JSONtoDataFrame").getOrCreate()
+
+    # API URL and parameters
+    url = "https://odds.p.rapidapi.com/v4/sports/upcoming/odds"
+    querystring = {"regions": "us", "oddsFormat": "decimal", "markets": "h2h,spreads", "dateFormat": "iso"}
+    headers = {
+        "x-rapidapi-key": "2042597c49msh48c7d7e021502ccp1e7bdcjsn91011bd9263b",
+        "x-rapidapi-host": "odds.p.rapidapi.com"
+    }
+
+    # Make the API request and get the response
+    response = requests.get(url, headers=headers, params=querystring)
+    json_data = response.json()
+
+    # Convert the JSON data to RDD and then to DataFrame
+    rdd = spark.sparkContext.parallelize([Row(**item) for item in json_data])
+    raw_df = spark.createDataFrame(rdd)
+
+    # Print schema to check the structure of 'bookmakers' and 'markets'
+    raw_df.printSchema()
+
+    # Explode the 'bookmakers' array
+    exploded_bookmakers_df = raw_df.withColumn("exploded_bookmakers", explode(col("bookmakers")))
+
+    # Print schema to ensure 'exploded_bookmakers' contains the correct data type for 'markets'
+    exploded_bookmakers_df.printSchema()
+
+    # Explode the 'markets' array inside 'exploded_bookmakers'
+    exploded_markets_df = exploded_bookmakers_df.withColumn("exploded_markets", explode(col("exploded_bookmakers.markets")))
+
+    # Explode the 'outcomes' array inside 'markets'
+    exploded_outcomes_df = exploded_markets_df.withColumn("exploded_outcomes", explode(col("exploded_markets.outcomes")))
+
+    # Access individual fields within the exploded 'bookmakers', 'markets', and 'outcomes'
+    final_df = exploded_outcomes_df.select(
+        "id",
+        "sport_title",
+        "commence_time",
+        "home_team",
+        "away_team",
+        col("exploded_bookmakers.key").alias("bookmaker_name"),
+        col("exploded_bookmakers.title").alias("bookmaker_title"),
+        col("exploded_markets.key").alias("market_type"),
+        col("exploded_outcomes.name").alias("outcome_name"),
+        col("exploded_outcomes.price").alias("outcome_price")
+    )
+
+    # Show the final DataFrame
+    final_df.show(truncate=False)
+
+# Call the function
+get_data()
+    
 # Load data from S3 (Uncomment this when ready to process actual data)
 # datasource0 = glueContext.create_dynamic_frame.from_options(
 #     connection_type="s3", 
@@ -34,4 +94,5 @@ print("HELLO FROM GLUE!")
 # )
 
 # Commit the Glue job
+get_data()
 job.commit()
